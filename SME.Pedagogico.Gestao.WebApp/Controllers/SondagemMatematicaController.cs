f96bd8bf65323c0bdc23e5ec727228539f9c7e77
Mayke Medeiros Rezende <mayke.rezende@amcom.com.br>
2019-05-24 15:40:27 -0300

    sondagem matematica backend

---
+++ SME.Pedagogico.Gestao.WebApp/Controllers/SondagemMatematicaController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SME.Pedagogico.Gestao.Data.Contexts;
+using SME.Pedagogico.Gestao.Data.DTO;
+using System.Threading.Tasks;
+
+namespace SME.Pedagogico.Gestao.WebApp.Controllers
+{
+    [Authorize]
+    [Produces("application/json")]
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class SondagemMatematicaController : Controller
+    {
+        #region ==================== ATTRIBUTES ====================
+
+        private readonly SMEManagementContext _db;
+
+        #endregion
+
+        #region ==================== CONSTRUCTORS ====================
+
+        /// <summary>
+        /// Construtor padrão para o SondagemMatematicaController, faz injeção de dependências SMEManagementContext.
+        /// </summary>
+        /// <param name="db">Depêndencia de dataContext (SMEManagementContext)</param>
+        public SondagemMatematicaController(SMEManagementContext db)
+        {
+            _db = db;
+        }
+
+        #endregion
+
+        #region ==================== METHODS ====================
+
+        #region -------------------- PRIVATE --------------------
+        #endregion
+
+        #region -------------------- PUBLIC --------------------
+
+        /// <summary>
+        /// Método para fazer a sondagem de matemática por ordem.
+        /// </summary>
+        /// <param name="credential">Objeto que contém informações da credencial do usuário, neste caso específico é necessário o atributo username e password</param>
+        /// <returns>Token, Sessão e RefreshToken gerado à partir das informações do usuário encontrado, caso não seja encontrado nenhum usuário correspondente à credencial, o método retorna usuário não autorizado.</returns>
+        [HttpPost]
+        public async Task<ActionResult<string>> SondagemMatematicaOrdem([FromBody]SondagemMatematicaOrdemDTO dadosSondagem)
+        {
+            return default;
+        }
+
+        #endregion
+
+        #endregion
+    }
+}
