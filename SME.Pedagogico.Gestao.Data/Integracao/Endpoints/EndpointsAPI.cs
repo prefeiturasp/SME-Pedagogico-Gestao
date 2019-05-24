9cce28e1907d565e83cc751f49e425af9ba8766f
Eleriane Costa <eleriane.costa@amcom.com.br>
2019-05-24 15:00:36 -0300

    Desenvolvimento das chamadas da API Integração. - Implementação do método HttpHelper - Chamada dos métodos de Cargos - Chamada dos métodos de Turma - Chamada dos métodos de Professores

---
+++ SME.Pedagogico.Gestao.Data/Integracao/Endpoints/EndpointsAPI.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SME.Pedagogico.Gestao.Data.Integracao.Endpoints
+{
+    public class EndpointsAPI
+    {
+        private string baseEndPoint = "";
+        private string buscaAlunosNaTurma = "";
+
+
+        public string BaseEndpoint { get; set; } = "http://10.50.0.196:81/api/";
+
+        public string BuscaAlunosNaTurma { get; set; } = "turmas/{0}/alunos/anosLetivos/{1}";
+
+        public string BuscaCargos { get; set; } = "cargos";
+
+        //professores/{codigoRF}/escolas/{codigoUE}/anos_letivos/{anoLetivo}
+        public string BuscaTurmasDeProfessores { get; set; } = "professores/{0}/escolas/{1}/anos_letivos/{2}";
+    }
+}
