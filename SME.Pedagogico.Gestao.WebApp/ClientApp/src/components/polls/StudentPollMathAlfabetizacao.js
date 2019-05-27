77555fc78393a55fa58de96e89bc22cc985615db
Massato Kanno <massato.kanno@amcom.com.br>
2019-05-27 16:58:11 -0300

    [Desenvolvimento] - inicio ajuste da sondagem de matemática para funcionar com o redux e api

--- SME.Pedagogico.Gestao.WebApp/ClientApp/src/components/polls/StudentPollMathAlfabetizacao.js
+++ SME.Pedagogico.Gestao.WebApp/ClientApp/src/components/polls/StudentPollMathAlfabetizacao.js
@@ -14,49 +14,49 @@ export default class StudentPollMathAlfabetizacao extends Component {
         return (
             <tr>
                 <th className="align-middle">
-                    <small className="ml-2 pr-4"><b>{this.props.student.sequence}</b></small><small>{this.props.student.name}</small>
+                    <small className="ml-2 pr-4"><b>{this.props.sequenceNumber}</b></small><small>{this.props.student.name}</small>
                 </th>
                 <th className="text-center border poll-select-container familiares_col">
-                    <PollSelectYesNo subjectName="mathalfabetizacao" sequence={this.props.student.sequence} name="t1b2" value={this.props.student.pollresults.mathalfabetizacao.t1b2} updatePollStudent={this.props.updatePollStudent} columnColor="bg-bluescale" />
+                    <PollSelectYesNo subjectName="mathalfabetizacao" sequence={this.props.student.studentCodeEol} name="familiares1S" value={this.props.student.familiares1S} updatePollStudent={this.props.updatePollStudent} columnColor="bg-bluescale" />
                 </th>
                 <th className="text-center border poll-select-container familiares_col">
-                    <PollSelectYesNo subjectName="mathalfabetizacao" sequence={this.props.student.sequence} name="t1b4" value={this.props.student.pollresults.mathalfabetizacao.t1b4} updatePollStudent={this.props.updatePollStudent} columnColor="bg-lila" />
+                    <PollSelectYesNo subjectName="mathalfabetizacao" sequence={this.props.student.studentCodeEol} name="familiares2S" value={this.props.student.familiares2S} updatePollStudent={this.props.updatePollStudent} columnColor="bg-lila" />
                 </th>
                 <th className="text-center border poll-select-container opacos_col">
-                    <PollSelectYesNo subjectName="mathalfabetizacao" sequence={this.props.student.sequence} name="t2b2" value={this.props.student.pollresults.mathalfabetizacao.t2b2} updatePollStudent={this.props.updatePollStudent} columnColor="bg-bluescale" />
+                    <PollSelectYesNo subjectName="mathalfabetizacao" sequence={this.props.student.studentCodeEol} name="opacos1S" value={this.props.student.opacos1S} updatePollStudent={this.props.updatePollStudent} columnColor="bg-bluescale" />
                 </th>
                 <th className="text-center border poll-select-container opacos_col">
-                    <PollSelectYesNo subjectName="mathalfabetizacao" sequence={this.props.student.sequence} name="t2b4" value={this.props.student.pollresults.mathalfabetizacao.t2b4} updatePollStudent={this.props.updatePollStudent} columnColor="bg-lila" />
+                    <PollSelectYesNo subjectName="mathalfabetizacao" sequence={this.props.student.studentCodeEol} name="opacos2S" value={this.props.student.opacos2S} updatePollStudent={this.props.updatePollStudent} columnColor="bg-lila" />
                 </th>
                 <th className="text-center border poll-select-container transparentes_col">
-                    <PollSelectYesNo subjectName="mathalfabetizacao" sequence={this.props.student.sequence} name="t3b2" value={this.props.student.pollresults.mathalfabetizacao.t3b2} updatePollStudent={this.props.updatePollStudent} columnColor="bg-bluescale" />
+                    <PollSelectYesNo subjectName="mathalfabetizacao" sequence={this.props.student.studentCodeEol} name="transparentes1S" value={this.props.student.transparentes1S} updatePollStudent={this.props.updatePollStudent} columnColor="bg-bluescale" />
                 </th>
                 <th className="text-center border poll-select-container transparentes_col">
-                    <PollSelectYesNo subjectName="mathalfabetizacao" sequence={this.props.student.sequence} name="t3b4" value={this.props.student.pollresults.mathalfabetizacao.t3b4} updatePollStudent={this.props.updatePollStudent} columnColor="bg-lila" />
+                    <PollSelectYesNo subjectName="mathalfabetizacao" sequence={this.props.student.studentCodeEol} name="transparentes2S" value={this.props.student.transparentes2S} updatePollStudent={this.props.updatePollStudent} columnColor="bg-lila" />
                 </th>
                 <th className="text-center border poll-select-container zero_col">
-                    <PollSelectYesNo subjectName="mathalfabetizacao" sequence={this.props.student.sequence} name="t4b2" value={this.props.student.pollresults.mathalfabetizacao.t4b2} updatePollStudent={this.props.updatePollStudent} columnColor="bg-bluescale" />
+                    <PollSelectYesNo subjectName="mathalfabetizacao" sequence={this.props.student.studentCodeEol} name="terminamzero1S" value={this.props.student.terminamzero1S} updatePollStudent={this.props.updatePollStudent} columnColor="bg-bluescale" />
                 </th>
                 <th className="text-center border poll-select-container zero_col">
-                    <PollSelectYesNo subjectName="mathalfabetizacao" sequence={this.props.student.sequence} name="t4b4" value={this.props.student.pollresults.mathalfabetizacao.t4b4} updatePollStudent={this.props.updatePollStudent} columnColor="bg-lila" />
+                    <PollSelectYesNo subjectName="mathalfabetizacao" sequence={this.props.student.studentCodeEol} name="terminamzero2S" value={this.props.student.terminamzero2S} updatePollStudent={this.props.updatePollStudent} columnColor="bg-lila" />
                 </th>
                 <th className="text-center border poll-select-container algarismos_col">
-                    <PollSelectYesNo subjectName="mathalfabetizacao" sequence={this.props.student.sequence} name="t5b2" value={this.props.student.pollresults.mathalfabetizacao.t5b2} updatePollStudent={this.props.updatePollStudent} columnColor="bg-bluescale" />
+                    <PollSelectYesNo subjectName="mathalfabetizacao" sequence={this.props.student.studentCodeEol} name="algarismos1S" value={this.props.student.algarismos1S} updatePollStudent={this.props.updatePollStudent} columnColor="bg-bluescale" />
                 </th>
                 <th className="text-center border poll-select-container algarismos_col">
-                    <PollSelectYesNo subjectName="mathalfabetizacao" sequence={this.props.student.sequence} name="t5b4" value={this.props.student.pollresults.mathalfabetizacao.t5b4} updatePollStudent={this.props.updatePollStudent} columnColor="bg-lila" />
+                    <PollSelectYesNo subjectName="mathalfabetizacao" sequence={this.props.student.studentCodeEol} name="algarismos2S" value={this.props.student.algarismos2S} updatePollStudent={this.props.updatePollStudent} columnColor="bg-lila" />
                 </th>
                 <th className="text-center border poll-select-container processo_col">
-                    <PollSelectYesNo subjectName="mathalfabetizacao" sequence={this.props.student.sequence} name="t6b2" value={this.props.student.pollresults.mathalfabetizacao.t6b2} updatePollStudent={this.props.updatePollStudent} columnColor="bg-bluescale" />
+                    <PollSelectYesNo subjectName="mathalfabetizacao" sequence={this.props.student.studentCodeEol} name="processo1S" value={this.props.student.processo1S} updatePollStudent={this.props.updatePollStudent} columnColor="bg-bluescale" />
                 </th>
                 <th className="text-center border poll-select-container processo_col">
-                    <PollSelectYesNo subjectName="mathalfabetizacao" sequence={this.props.student.sequence} name="t6b4" value={this.props.student.pollresults.mathalfabetizacao.t6b4} updatePollStudent={this.props.updatePollStudent} columnColor="bg-lila" />
+                    <PollSelectYesNo subjectName="mathalfabetizacao" sequence={this.props.student.studentCodeEol} name="processo2S" value={this.props.student.processo2S} updatePollStudent={this.props.updatePollStudent} columnColor="bg-lila" />
                 </th>
                 <th className="text-center border poll-select-container zeros_col">
-                    <PollSelectYesNo subjectName="mathalfabetizacao" sequence={this.props.student.sequence} name="t7b2" value={this.props.student.pollresults.mathalfabetizacao.t7b2} updatePollStudent={this.props.updatePollStudent} columnColor="bg-bluescale" />
+                    <PollSelectYesNo subjectName="mathalfabetizacao" sequence={this.props.student.studentCodeEol} name="zerointercalados1S" value={this.props.student.zerointercalados1S} updatePollStudent={this.props.updatePollStudent} columnColor="bg-bluescale" />
                 </th>
                 <th className="text-center border poll-select-container zeros_col">
-                    <PollSelectYesNo subjectName="mathalfabetizacao" sequence={this.props.student.sequence} name="t7b4" value={this.props.student.pollresults.mathalfabetizacao.t7b4} updatePollStudent={this.props.updatePollStudent} columnColor="bg-lila" />
+                    <PollSelectYesNo subjectName="mathalfabetizacao" sequence={this.props.student.studentCodeEol} name="zerointercalados2S" value={this.props.student.zerointercalados2S} updatePollStudent={this.props.updatePollStudent} columnColor="bg-lila" />
                 </th>
             </tr>
         );
--- SME.Pedagogico.Gestao.WebApp/ClientApp/src/store/Poll.js
+++ SME.Pedagogico.Gestao.WebApp/ClientApp/src/store/Poll.js
@@ -1,9 +1,13 @@
 ﻿export const types = {
     SET_POLL_INFO:"SET_POLL_INFO",
-    GET_POLL_PORTUGUESE_STUDENTS: "GET_POLL_PORTUGUESE_STUDENTS",
     UPDATE_POLL_STUDENTS: "UPDATE_POLL_STUDENTS",
+
+    GET_POLL_PORTUGUESE_STUDENTS: "GET_POLL_PORTUGUESE_STUDENTS",
     SAVE_POLL_PORTUGUESE: "SAVE_POLL_PORTUGUESE",
 
+    
+    GET_POLL_MATH_NUMBERS_STUDENTS: "GET_POLL_MATH_NUMBERS_STUDENTS",
+    SAVE_POLL_MATH_NUMBERS_STUDENTS: "SAVE_POLL_MATH_NUMBERS_STUDENTS",
 
 }
 
@@ -16,15 +20,21 @@ const initialState = {
 }
 
 export const actionCreators = {
-    get_poll_portuguese_students: (classRoom) => ({ type: types.GET_POLL_PORTUGUESE_STUDENTS, classRoom }),
     set_poll_info: (pollSelected, pollTypeSelected, pollYear) => ({ type: types.SET_POLL_INFO, pollSelected, pollTypeSelected, pollYear }),
+
     update_poll_students: (pollstudents) => ({ type: types.UPDATE_POLL_STUDENTS, pollstudents }),
-    save_poll_student: (pollstudents) => ({type:types.SAVE_POLL_PORTUGUESE,pollstudents}),
+
+    get_poll_portuguese_students: (classRoom) => ({ type: types.GET_POLL_PORTUGUESE_STUDENTS, classRoom }),
+    save_poll_portuguese_student: (pollstudents) => ({ type: types.SAVE_POLL_PORTUGUESE, pollstudents }),
+
+    get_poll_math_numbers_students: () => ({ type: types.GET_POLL_MATH_NUMBERS_STUDENTS}),
 }
 
 export const reducer = (state, action, pollSelected, pollTypeSelected, pollYear, pollstudents) => {
     state = state || initialState;
     switch (action.type) {
+        case types.SET_POLL_INFO:
+            return ({ ...state, pollSelected: action.pollSelected, pollTypeSelected: action.pollTypeSelected, pollYear: action.pollYear });
         case types.GET_POLL_PORTUGUESE_STUDENTS:
             var students = [{
                 "name":"Eduarda dos Santos Costa",
@@ -68,11 +78,74 @@ export const reducer = (state, action, pollSelected, pollTypeSelected, pollYear,
         case types.SET_POLL_PORTUGUESE_STUDENTS:
             return ({...state,classRoom: action.data,
             });
-        case types.SET_POLL_INFO:
-            return ({ ...state, pollSelected: action.pollSelected, pollTypeSelected: action.pollTypeSelected, pollYear:action.pollYear });
         case types.UPDATE_POLL_STUDENTS:
             return ({...state, students:action.pollstudents
             });
+        case types.GET_POLL_MATH_NUMBERS_STUDENTS:
+            var students = [{
+                "name": "Eduarda dos Santos Costa",
+                "studentCodeEol": "1",
+                "sequenceNumber": "1",
+                "dreCodeEol": "4",
+                "schoolCodeEol": "44",
+                "classroomCodeEol": "33",
+                "schoolYear": "2019",
+                "yearClassroom": "1",
+
+                "familiares1S": "S",
+                "familiares2S": "",
+
+                "opacos1S": "S",
+                "opacos2S": "",
+
+                "transparentes1S": "N",
+                "transparentes2S": "",
+
+                "terminamzero1S": "S",
+                "terminamzero2S": "",
+
+                "algarismos1S": "S",
+                "algarismos2S": "N",
+
+                "processo1S": "S",
+                "processo2S": "",
+
+                "zerointercalados1S": "S",
+                "zerointercalados2S": "",
+                
+            },
+            {
+                "name": "José Santos Costa",
+                "studentCodeEol": "2",
+                "sequenceNumber": "2",
+                "dreCodeEol": "4",
+                "schoolCodeEol": "44",
+                "classroomCodeEol": "33",
+                "schoolYear": "2019",
+                "yearClassroom": "1",
+
+                "familiares1S": "S",
+                "familiares2S": "N",
+
+                "opacos1S": "S",
+                "opacos2S": "N",
+
+                "transparentes1S": "N",
+                "transparentes2S": "",
+
+                "terminamzero1S": "",
+                "terminamzero2S": "",
+
+                "algarismos1S": "",
+                "algarismos2S": "",
+
+                "processo1S": "",
+                "processo2S": "",
+
+                "zerointercalados1S": "",
+                "zerointercalados2S": "",
+            }]
+            return ({ ...state, classRoom: action.classRoom, students: students });
         default:
             return (state);
     }
