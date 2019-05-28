1e898573a22c93919890f48b0110e4505b567993
Eleriane Costa <eleriane.costa@amcom.com.br>
2019-05-28 15:32:40 -0300

    Desenvolvimento dos Métodos da API DREs

---
+++ SME.Pedagogico.Gestao.Data/Integracao/DREAPI.cs
@@ -0,0 +1,74 @@
+﻿using SME.Pedagogico.Gestao.Data.Integracao.DTO.RetornoQueryDTO;
+using SME.Pedagogico.Gestao.Data.Integracao.Endpoints;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SME.Pedagogico.Gestao.Data.Integracao
+{
+    public class DREAPI
+    { 
+        private readonly EndpointsAPI endpointsAPI;
+
+        public DREAPI(EndpointsAPI endpointsAPI)
+        {
+            this.endpointsAPI = endpointsAPI;
+        } 
+
+        public async Task<List<DREsDTO>> GetDres( string token)
+        {
+            var url = HttpHelper.ConstroiURL(endpointsAPI.BaseEndpoint, endpointsAPI.BuscaDres);
+
+            return await HttpHelper
+                .GetAsync<List<DREsDTO>>
+                      (token, string.Format(url));
+        }
+         
+        public async Task<List<DREsDTO>> GetDresPorCodigo(string codigoDRE, string token)
+        {
+            var url = HttpHelper.ConstroiURL(endpointsAPI.BaseEndpoint, endpointsAPI.BuscaDresPorCodigo);
+
+            return await HttpHelper
+                .GetAsync<List<DREsDTO>>
+                      (token, string.Format(url, codigoDRE));
+        }
+
+        public async Task<List<EscolasPorDREDTO>> GetEscolasPorDREPorTipoEscola(string codigoDRE, string tipoEscola, string token)
+        {
+            var url = HttpHelper.ConstroiURL(endpointsAPI.BaseEndpoint, endpointsAPI.BuscaEscolasPorDREPorTipoEscola);
+
+            return await HttpHelper
+                .GetAsync<List<EscolasPorDREDTO>>
+                      (token, string.Format(url, codigoDRE, tipoEscola));
+        }
+         
+        public async Task<List<EscolasPorDREDTO>> GetEscolasPorDRE(string codigoDRE, string token)
+        {
+            var url = HttpHelper.ConstroiURL(endpointsAPI.BaseEndpoint, endpointsAPI.BuscaEscolasPorDre);
+
+            return await HttpHelper
+                .GetAsync<List<EscolasPorDREDTO>>
+                      (token, string.Format(url, codigoDRE));
+
+        }
+
+        public async Task<List<EscolasPorDREDTO>> GetEscolasPorTipoEscola(string tipoEscola, string token)
+        {
+            var url = HttpHelper.ConstroiURL(endpointsAPI.BaseEndpoint, endpointsAPI.BuscaEscolasPorTipoEscola);
+
+            return await HttpHelper
+                .GetAsync<List<EscolasPorDREDTO>>
+                      (token, string.Format(url, tipoEscola));
+        }
+
+        public async Task<List<SubprefeituraDTO>> GetSubprefeituraPor(string codigoDRE, string token)
+        {
+            var url = HttpHelper.ConstroiURL(endpointsAPI.BaseEndpoint, endpointsAPI.BuscaSubprefeituraPor);
+
+            return await HttpHelper
+                .GetAsync<List<SubprefeituraDTO>>
+                      (token, string.Format(url, codigoDRE));
+        }
+    }
+}
