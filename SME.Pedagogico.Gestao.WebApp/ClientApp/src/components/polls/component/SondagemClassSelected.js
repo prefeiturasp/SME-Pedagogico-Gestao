94f2d606c6c405c04dc4a7e5c208db436880e56c
Massato Kanno <massato.kanno@amcom.com.br>
2019-06-04 14:23:55 -0300

    [Ajuste] - ajuste sondagem matematica no clique duplo para carregar

--- SME.Pedagogico.Gestao.WebApp/ClientApp/src/components/polls/component/SondagemClassSelected.js
+++ SME.Pedagogico.Gestao.WebApp/ClientApp/src/components/polls/component/SondagemClassSelected.js
@@ -12,6 +12,17 @@ class SondagemClassSelected extends Component {
         this.btnCheckNumeric = this.btnCheckNumeric.bind(this);
         this.btnCheckCA = this.btnCheckCA.bind(this);
         this.btnCheckCM = this.btnCheckCM.bind(this);
+
+        if (this.props.poll.pollTypeSelected === "Numeric") {
+
+            this.props.pollMethods.get_poll_math_numbers_students(this.props.poll.selectedFilter);
+        } else if (this.props.poll.pollTypeSelected === "CA") {
+
+            this.props.pollMethods.get_poll_math_ca_students(this.props.poll.selectedFilter);
+        } else if (this.props.poll.pollTypeSelected === "CM") {
+
+            this.props.pollMethods.get_poll_math_cm_students(this.props.poll.selectedFilter);
+        }
     }
 
     btnCheckNumeric() {
@@ -56,17 +67,17 @@ class SondagemClassSelected extends Component {
         this.props.pollMethods.set_poll_type_selected(e.currentTarget.value);
         var classRoomMock = this.props.poll.selectedFilter;
 
-        this.props.pollMethods.set_poll_list_initial_state();
-        if (this.props.poll.pollTypeSelected === "Numeric") {
+        //this.props.pollMethods.set_poll_list_initial_state();
+        //if (this.props.poll.pollTypeSelected === "Numeric") {
             
-            this.props.pollMethods.get_poll_math_numbers_students(classRoomMock);
-        } else if (this.props.poll.pollTypeSelected === "CA") {
+        //    this.props.pollMethods.get_poll_math_numbers_students(classRoomMock);
+        //} else if (this.props.poll.pollTypeSelected === "CA") {
             
-            this.props.pollMethods.get_poll_math_ca_students(classRoomMock);
-        } else if (this.props.poll.pollTypeSelected === "CM") {
+        //    this.props.pollMethods.get_poll_math_ca_students(classRoomMock);
+        //} else if (this.props.poll.pollTypeSelected === "CM") {
             
-            this.props.pollMethods.get_poll_math_cm_students(classRoomMock);
-        } 
+        //    this.props.pollMethods.get_poll_math_cm_students(classRoomMock);
+        //} 
     }
     render() {
         var buttonRender;
