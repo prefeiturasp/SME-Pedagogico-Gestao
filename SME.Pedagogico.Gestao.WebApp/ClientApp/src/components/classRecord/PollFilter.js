8de60744b3a5a100bca3c46a05bfe5c6c8955f84
Daniel Matsumoto <daniel.matsumoto@amcom.com.br>
2019-06-04 09:40:57 -0300

    [Correção de Bugs] - Correção no filtro de selecionar turma na Sondagem

--- SME.Pedagogico.Gestao.WebApp/ClientApp/src/components/classRecord/PollFilter.js
+++ SME.Pedagogico.Gestao.WebApp/ClientApp/src/components/classRecord/PollFilter.js
@@ -10,11 +10,11 @@ class PollFilter extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            selectedDre: null,
-            selectedSchool: null,
-            selectedClassRoom: null,
+            selectedDre: "",
+            selectedSchool: "",
+            selectedClassRoom: "",
             yearClassroom: null,
-            classroom: null,
+            classroom: "",
         }
 
         this.SelectedDre = this.SelectedDre.bind(this);
@@ -29,7 +29,13 @@ class PollFilter extends Component {
         this.props.filterMethods.resetPollFilters();
     }
 
+    componentDidMount() {
+        this.props.filterMethods.getDre();
+    }
+
     SelectedDre(event) {
+        this.props.filterMethods.resetPollFilters();
+        this.props.filterMethods.getDre();
         var index = event.nativeEvent.target.selectedIndex;
         var label = event.nativeEvent.target[index].value;
 
@@ -39,7 +45,21 @@ class PollFilter extends Component {
         }
 
         this.props.filterMethods.getSchool(schoolCode);
+        this.setState({
+            selectedDre: label,
+            selectedSchool: "",
+            selectedClassRoom: "",
+            yearClassroom: null,
+            classroom: "",
+        });
 
+        if (label === "todas") {
+            this.props.filterMethods.activeClassroom("todas");
+            this.props.filterMethods.getClassroom({
+                schoolCodeEol: "todas",
+                schoolYear: "2019",
+            });
+        }
     }
 
     SelectedClassRoom(event) {
@@ -52,11 +72,16 @@ class PollFilter extends Component {
         this.props.filterMethods.activeClassroom(codeClassRoom);
 
         this.setState({
-            classroom: event.target[index].innerText.substring(0, 1)
+            classroom: event.target[index].innerText.substring(0, 1),
+            selectedClassRoom: codeClassRoom,
         });
     }
 
     SelectedSchool(event) {
+        this.props.filterMethods.getSchool({
+            dreCodeEol: this.state.selectedDre,
+            schoolYear: "2019",
+        });
 
         var index = event.nativeEvent.target.selectedIndex;
         var label = event.nativeEvent.target[index].value;
@@ -67,15 +92,18 @@ class PollFilter extends Component {
         }
 
         this.props.filterMethods.getClassroom(classRoomFilter);
-    }
-
-    componentDidMount() {
-        this.props.filterMethods.getDre();
+        this.setState({
+            selectedSchool: label,
+            selectedClassRoom: "",
+            yearClassroom: null,
+            classroom: "",
+        });
     }
 
     getClassroom(event) {
         this.setState({
-            classroom: event.target.value
+            classroom: event.target.value,
+            selectedClassRoom: "",
         });
     }
 
@@ -111,8 +139,8 @@ class PollFilter extends Component {
         const { selectedSchool } = this.state;
         const { selectedClassRoom } = this.state;
 
-        const listDresOptions = [];
-        const listSchoolOptions = [];
+        const listDresOptions = [{ label: "Todas", value: "todas" }];
+        const listSchoolOptions = [{ label: "Todas", value: "todas" }];
         const listClassRoomOptions = [];
 
 
@@ -123,44 +151,48 @@ class PollFilter extends Component {
             });
         }
 
+        if (selectedDre !== "todas") {
+            for (var item in this.props.filters.scholls) {
+                listSchoolOptions.push({
+                    value: this.props.filters.scholls[item].codigoEscola,
+                    label: this.props.filters.scholls[item].nomeEscola,
+                });
 
-        for (var item in this.props.filters.scholls) {
-            listSchoolOptions.push({
-                value: this.props.filters.scholls[item].codigoEscola,
-                label: this.props.filters.scholls[item].nomeEscola,
-            });
+            }
 
-        }
-        if (this.props.filters.listClassRoom !== [] && this.props.filters.listClassRoom !== null) {
-            if (this.state.classroom !== null)
-                for (var item in this.props.filters.listClassRoom) {
-                    if (this.props.filters.listClassRoom[item].nomeTurma.startsWith(this.state.classroom))
-                        listClassRoomOptions.push({
-                            value: this.props.filters.listClassRoom[item].codigoTurma,
-                            label: this.props.filters.listClassRoom[item].nomeTurma,
-                        });
+            if (selectedSchool !== "todas") {
+                if (this.props.filters.listClassRoom !== [] && this.props.filters.listClassRoom !== null && this.props.filters.listClassRoom.length > 1) {
+                    if (this.state.classroom !== null)
+                        for (var item in this.props.filters.listClassRoom) {
+                            if (this.props.filters.listClassRoom[item].nomeTurma.startsWith(this.state.classroom))
+                                listClassRoomOptions.push({
+                                    value: this.props.filters.listClassRoom[item].codigoTurma,
+                                    label: this.props.filters.listClassRoom[item].nomeTurma,
+                                });
+                        }
+                    else
+                        for (var item in this.props.filters.listClassRoom) {
+                            listClassRoomOptions.push({
+                                value: this.props.filters.listClassRoom[item].codigoTurma,
+                                label: this.props.filters.listClassRoom[item].nomeTurma,
+                            });
+                        }
                 }
-            else
-                for (var item in this.props.filters.listClassRoom) {
-                    listClassRoomOptions.push({
-                        value: this.props.filters.listClassRoom[item].codigoTurma,
-                        label: this.props.filters.listClassRoom[item].nomeTurma,
-                    });
-                }
-        }
+            }
 
-        var yearClassrooms = [];
+            var yearClassrooms = [];
 
-        if (this.props.filters.listClassRoom !== null) {
-            var temp = this.props.filters.listClassRoom;
-            var uniques = [];
+            if (this.props.filters.listClassRoom !== null) {
+                var temp = this.props.filters.listClassRoom;
+                var uniques = [];
 
-            for (var i = 0; i < temp.length; i++) {
-                var classroom = temp[i].nomeTurma.substring(0, 1);
+                for (var i = 0; i < temp.length; i++) {
+                    var classroom = temp[i].nomeTurma.substring(0, 1);
 
-                if (uniques.indexOf(classroom) === -1) {
-                    yearClassrooms.push({ label: classroom, value: classroom });
-                    uniques.push(classroom);
+                    if (uniques.indexOf(classroom) === -1) {
+                        yearClassrooms.push({ label: classroom, value: classroom });
+                        uniques.push(classroom);
+                    }
                 }
             }
         }
@@ -174,11 +206,11 @@ class PollFilter extends Component {
                 <SelectChangeColor className="col-4" defaultText="Selecione a DRE"
                     value={selectedDre} options={listDresOptions} onChange={this.SelectedDre} />
                 <div className="px-2"></div>
-                <SelectChangeColor className="col-4" value={selectedSchool} defaultText="Escola" options={listSchoolOptions} onChange={this.SelectedSchool} />
+                <SelectChangeColor className="col-4" value={selectedSchool} defaultText="Escola" options={listSchoolOptions} onChange={this.SelectedSchool} resetColor={selectedSchool === "" ? true : false} />
                 <div className="px-2"></div>
-                <SelectChangeColor className="" defaultText="Ano" options={yearClassrooms} onChange={this.getClassroom} />
+                <SelectChangeColor className="" value={this.state.classroom} defaultText="Ano" options={yearClassrooms} onChange={this.getClassroom} activeColor={this.state.classroom === "" ? false : true} resetColor={this.state.classroom === "" ? true : false} />
                 <div className="px-2"></div>
-                <SelectChangeColor className="" value={selectedClassRoom} defaultText="Turma" options={listClassRoomOptions} onChange={this.SelectedClassRoom} />
+                <SelectChangeColor className="" value={selectedClassRoom} defaultText="Turma" options={listClassRoomOptions} onChange={this.SelectedClassRoom} resetColor={selectedClassRoom === "" ? true : false} />
                 <div className="px-2"></div>
                 <CircleButton iconClass="fas fa-search" onClick={this.setSelectedFilter} disabled={!this.checkDisabledButton()} />
             </div>
