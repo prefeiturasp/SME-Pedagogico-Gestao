61fc3bf5e63a47c60f237dc6837baefb94ac7700
Caique Siqueira <latorrecaique@gmail.com>
2020-09-10 16:23:35 -0300

    AB#23006

--- SME.Pedagogico.Gestao.WebApp/ClientApp/src/store/SondagemPortuguesStore.js
+++ SME.Pedagogico.Gestao.WebApp/ClientApp/src/store/SondagemPortuguesStore.js
@@ -2,103 +2,145 @@ import { type } from "jquery";
 import { isGetAccessor } from "typescript";
 
 export const types = {
-  SELECIONAR_GRUPO: "SELECIONAR_GRUPO",
-  SETAR_GRUPOS: "SETAR_GRUPOS",
-  SETAR_ORDEM_SELECIONADA: "SETAR_ORDEM_SELECIONADA",
-  SETAR_ALUNOS: "SETAR_ALUNOS",
-  SETAR_PERIODOS: "SETAR_PERIODOS",
-  SETAR_PERGUNTAS: "SETAR_PERGUNTAS",
-  ATUALIZAR_RESPOSTA: "ATUALIZAR_RESPOSTA",
-};
+    SELECIONAR_GRUPO: "SELECIONAR_GRUPO",
+    SETAR_GRUPOS: "SETAR_GRUPOS",
+    SETAR_ORDEM_SELECIONADA: "SETAR_ORDEM_SELECIONADA",
+    SETAR_ALUNOS: "SETAR_ALUNOS",
+    SETAR_PERIODOS: "SETAR_PERIODOS",
+    SETAR_PERGUNTAS: "SETAR_PERGUNTAS",
+    ATUALIZAR_RESPOSTA: "ATUALIZAR_RESPOSTA",
+    //
+    LISTAR_GRUPOS: "LISTAR_GRUPOS",
+    LISTAR_COMPONENTE_CURRICULAR: "LISTAR_COMPONENTE_CURRICULAR",
+    LISTAR_BIMESTRES: "LISTAR_BIMESTRES",
+    LISTAR_PERGUNTAS: "LISTAR_PERGUNTAS",
+    LISTAR_SEQUENCIA_ORDENS: "LISTAR_SEQUENCIA_ORDENS",
+    LISTAR_ALUNOS_PORTUGUES: "LISTAR_ALUNOS_PORTUGUES",
+    SALVAR_SONDAGEM_PORTUGUES: "SALVAR_SONDAGEM_PORTUGUES"
+}
 
 const initialState = {
-  grupoSelecionado: null,
-  grupos: [],
-  ordemSelecionada: null,
-  perguntas: [],
-  alunos: [],
-  periodos: [],
+    grupoSelecionado: null,
+    grupos: [],
+    ordemSelecionada: null,
+    perguntas: [],
+    alunos: [],
+    periodos: [],
+    sequenciaOrdens: [],
+    componenteCurricular: null
 };
 
 export const actionCreators = {
-  selecionar_grupo: (grupo) => ({
-    type: types.SELECIONAR_GRUPO,
-    payload: grupo,
-  }),
-  setar_grupos: (grupos) => ({
-    type: types.SETAR_GRUPOS,
-    payload: grupos,
-  }),
-  setar_ordem_selecionada: (ordemId) => ({
-    type: types.SETAR_ORDEM_SELECIONADA,
-    payload: ordemId,
-  }),
-  setar_alunos: (alunos) => ({
-    type: types.SETAR_ALUNOS,
-    payload: alunos,
-  }),
-  setar_periodos: (periodos) => ({
-    type: types.SETAR_PERIODOS,
-    payload: periodos,
-  }),
-  setar_perguntas: (perguntas) => ({
-    type: types.SETAR_PERGUNTAS,
-    payload: perguntas,
-  }),
-  atualizar_resposta: (atualizarDto) => ({
-    type: types.ATUALIZAR_RESPOSTA,
-    payload: atualizarDto,
-  })
+    listarGrupos: () => ({ type: types.LISTAR_GRUPOS }),
+    listarComponenteCurricular: () => ({ type: types.LISTAR_COMPONENTE_CURRICULAR }),
+    listarBimestres: () => ({ type: types.LISTAR_BIMESTRES }),
+    listarPerguntas: (sequenciaOrdem) => ({
+        type: types.LISTAR_PERGUNTAS,
+        payload: sequenciaOrdem,
+    }),
+    listarSequenciaOrdens: (filtros) => ({
+        type: types.LISTAR_SEQUENCIA_ORDENS,
+        payload: filtros,
+    }),
+
+    listarAlunosPortugues: (filtros) => ({
+        type: types.LISTAR_ALUNOS_PORTUGUES,
+        payload: filtros,
+    }),
+
+    salvarSondagemPortugues: (alunos, filtro) => ({
+        type: types.SALVAR_SONDAGEM_PORTUGUES,
+        payload: { alunos, filtro },
+    }),
+
+    //
+    selecionar_grupo: (grupo) => ({
+        type: types.SELECIONAR_GRUPO,
+        payload: grupo,
+    }),
+    setar_grupos: (grupos) => ({
+        type: types.SETAR_GRUPOS,
+        payload: grupos,
+    }),
+    setar_ordem_selecionada: (ordemId) => ({
+        type: types.SETAR_ORDEM_SELECIONADA,
+        payload: ordemId,
+    }),
+    setar_alunos: (alunos) => ({
+        type: types.SETAR_ALUNOS,
+        payload: alunos,
+    }),
+    setar_periodos: (periodos) => ({
+        type: types.SETAR_PERIODOS,
+        payload: periodos,
+    }),
+   
+    atualizar_resposta: (atualizarDto) => ({
+        type: types.ATUALIZAR_RESPOSTA,
+        payload: atualizarDto,
+    })
 };
 
 export const reducer = (state, action) => {
-  state = state || initialState;
+    state = state || initialState;
 
-  switch (action.type) {
-    case types.SELECIONAR_GRUPO:
-      return { ...state, grupoSelecionado: action.payload };
-    case types.SETAR_GRUPOS:
-      return { ...state, grupos: action.payload };
-    case types.SETAR_ORDEM_SELECIONADA:
-      return { ...state, ordemSelecionada: action.payload };
-    case types.SETAR_ALUNOS:
-      return { ...state, alunos: action.payload };
-    case types.SETAR_PERIODOS:
-      return { ...state, periodos: action.payload };
-    case types.SETAR_PERGUNTAS:
-      return { ...state, perguntas: action.payload };
-    case types.ATUALIZAR_RESPOSTA:
-      const alunos = Object.assign([], state.alunos);
+    switch (action.type) {
+        case types.SELECIONAR_GRUPO:
+            return { ...state, grupoSelecionado: action.payload };
+        case types.SETAR_GRUPOS:
+            return { ...state, grupos: action.payload };
+        case types.SETAR_COMPONENTE_CURRICULAR:
+            return { ...state, componenteCurricular: action.payload };
+        case types.SETAR_BIMESTRES:
+            return { ...state, periodos: action.payload };
+        case types.SETAR_PERGUNTAS:
+            return { ...state, perguntas: action.payload };
+        case types.SETAR_SEQUENCIA_ORDENS:
+            return { ...state, sequenciaOrdens: action.payload }; 
+        case types.SETAR_ALUNOS_PORTUGUES:
+            return { ...state, alunos: action.payload }; 
+           
+            
+      //
 
-      const alunoIndex = alunos.findIndex(aluno => aluno.codigoAluno === action.payload.alunoId);
 
-      if (alunoIndex < 0)
-        return state;
+        case types.SETAR_ORDEM_SELECIONADA:
+            return { ...state, ordemSelecionada: action.payload };
+        case types.SETAR_ALUNOS:
+            return { ...state, alunos: action.payload };
 
-      const aluno = alunos[alunoIndex];
+        case types.ATUALIZAR_RESPOSTA:
+            const alunos = Object.assign([], state.alunos);
 
-      const respostaIndex = aluno.respostas ? aluno.respostas
-        .findIndex(resposta => resposta.pergunta === action.payload.perguntaId
-          && action.payload.periodoId) : -1;
+            const alunoIndex = alunos.findIndex(aluno => aluno.codigoAluno === action.payload.alunoId);
 
-      const resposta = respostaIndex < 0 ? {
-        periodoId: action.payload.periodoId,
-        pergunta: action.payload.perguntaId,
-        resposta: action.payload.respostaId,
-      } : aluno.respostas[respostaIndex];
+            if (alunoIndex < 0)
+                return state;
 
-      resposta.resposta = action.payload.respostaId;
+            const aluno = alunos[alunoIndex];
 
-      if (!aluno.respostas)
-        alunos[alunoIndex].respostas = [];
+            const respostaIndex = aluno.respostas ? aluno.respostas
+                .findIndex(resposta => resposta.pergunta === action.payload.perguntaId
+                    && action.payload.periodoId) : -1;
 
-      if (respostaIndex < 0)
-        alunos[alunoIndex].respostas.push(resposta);
-      else
-        alunos[alunoIndex].respostas[respostaIndex] = resposta;
+            const resposta = respostaIndex < 0 ? {
+                periodoId: action.payload.periodoId,
+                pergunta: action.payload.perguntaId,
+                resposta: action.payload.respostaId,
+            } : aluno.respostas[respostaIndex];
 
-      return { ...state, alunos };
-    default:
-      return state;
-  }
+            resposta.resposta = action.payload.respostaId;
+
+            if (!aluno.respostas)
+                alunos[alunoIndex].respostas = [];
+
+            if (respostaIndex < 0)
+                alunos[alunoIndex].respostas.push(resposta);
+            else
+                alunos[alunoIndex].respostas[respostaIndex] = resposta;
+
+            return { ...state, alunos };
+        default:
+            return state;
+    }
 };
