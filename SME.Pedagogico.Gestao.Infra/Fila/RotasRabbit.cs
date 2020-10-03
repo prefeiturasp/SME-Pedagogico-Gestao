adb88623228ab59b618a67d3c13739be3b4a2f50
Marcos Lobo <marcos.lobo.nit@gmail.com>
2020-10-03 11:19:30 -0300

    Removendo rotas não utilizadas pelo sondagem

--- SME.Pedagogico.Gestao.Infra/Fila/RotasRabbit.cs
+++ SME.Pedagogico.Gestao.Infra/Fila/RotasRabbit.cs
@@ -10,14 +10,6 @@
 
         public static string RotaRelatoriosSolicitados => "relatorios.solicitados";
         public static string RotaRelatoriosProntos => "relatorios.prontos";
-
-        public static string RotaExcluirAulaRecorrencia => "aula.excluir.recorrencia";
-        public static string RotaInserirAulaRecorrencia => "aula.cadastrar.recorrencia";
-        public static string RotaAlterarAulaRecorrencia => "aula.alterar.recorrencia";
-        public static string RotaNotificacaoUsuario => "notificacao.usuario";
-        public static string RotaNotificacaoExclusaoAulasComFrequencia => "notificacao.aulas.exclusao.frequencia";
-        public static string RotaCriarAulasInfatilAutomaticamente => "aulas.infantil.criar";
-        public static string RotaSincronizarAulasInfatil => "aulas.infantil.sincronizar";
         public static string RotaRelatorioComErro => "relatorios.erro";
         public static string RotaRelatorioCorrelacaoCopiar => "relatorios.correlacao.copiar";
         public static string RotaRelatorioCorrelacaoInserir => "relatorios.correlacao.inserir";
