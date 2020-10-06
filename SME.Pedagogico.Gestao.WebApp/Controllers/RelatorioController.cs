c2a58861593058be658fb9ab8bde594b712dcdf7
Marcos Lobo <marcos.lobo.nit@gmail.com>
2020-10-06 14:04:29 -0300

    Controller dos relatórios

---
+++ SME.Pedagogico.Gestao.WebApp/Controllers/RelatorioController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SME.Pedagogico.Gestao.WebApp.Controllers
+{
+    [ApiController]
+    [Route("api/v1/relatorios")]
+    public class RelatorioController : ControllerBase
+    {
+        //[HttpPost]
+        //public async Task<IActionResult> GerarConsolidadoMatematicaNumerosAutoral([FromServices]IMediator mediator)
+        //{
+
+        //}
+    }
+}
