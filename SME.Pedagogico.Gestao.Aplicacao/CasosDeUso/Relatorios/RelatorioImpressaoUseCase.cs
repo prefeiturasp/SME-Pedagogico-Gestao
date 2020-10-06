0abc41e82f3ad5c0af1d4a48c971759a94a00e22
Marcos Lobo <marcos.lobo.nit@gmail.com>
2020-10-06 17:25:50 -0300

    Use case de impressão de relatórios Removendo códigos não utilizados

---
+++ SME.Pedagogico.Gestao.Aplicacao/CasosDeUso/Relatorios/RelatorioImpressaoUseCase.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+using SME.Pedagogico.Gestao.Infra;
+using System;
+using System.Threading.Tasks;
+
+namespace SME.Pedagogico.Gestao.Aplicacao
+{
+    public class RelatorioImpressaoUseCase : IRelatorioImpressaoUseCase
+    {
+        private readonly IMediator mediator;
+
+        public RelatorioImpressaoUseCase(IMediator mediator)
+        {
+            this.mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+        }
+        public async Task Executar(RelatorioImpressaoFiltroDto filtros)
+        {
+
+            if (filtros.ComponenteCurricular == ComponenteCurricularEnum.Matematica)
+            {
+                if (filtros.Proficiencia == ProficienciaEnum.CampoAditivo || filtros.Proficiencia == ProficienciaEnum.CampoMultiplicativo)
+                {
+                    await mediator.Send(new GerarRelatorioCommand(TipoRelatorio.RelatorioMatetimaticaPorTurma, filtros, filtros.UsuarioRF));
+                }
+            }
+
+
+        }
+    }
+}
--- SME.Pedagogico.Gestao.WebApp/Controllers/RelatorioController.cs
+++ SME.Pedagogico.Gestao.WebApp/Controllers/RelatorioController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using SME.Pedagogico.Gestao.Aplicacao;
+using SME.Pedagogico.Gestao.Infra;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,10 +13,12 @@ namespace SME.Pedagogico.Gestao.WebApp.Controllers
     [Route("api/v1/relatorios")]
     public class RelatorioController : ControllerBase
     {
-        //[HttpPost]
-        //public async Task<IActionResult> GerarConsolidadoMatematicaNumerosAutoral([FromServices]IMediator mediator)
-        //{
-
-        //}
+        [HttpPost]
+        public async Task<IActionResult> Gerar([FromServices]IMediator mediator,[FromBody]RelatorioImpressaoFiltroDto filtros,
+            [FromServices]IRelatorioImpressaoUseCase relatorioMatematicaNumerosAutoralConsolidadoUseCase)
+        {
+            await relatorioMatematicaNumerosAutoralConsolidadoUseCase.Executar(filtros);
+            return Ok();
+        }
     }
 }
