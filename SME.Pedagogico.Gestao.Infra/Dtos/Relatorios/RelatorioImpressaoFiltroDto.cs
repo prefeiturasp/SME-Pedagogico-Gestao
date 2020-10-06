f1c3e3ecc06dba425d1afb90a034efe5c2ad2ad7
Marcos Lobo <marcos.lobo.nit@gmail.com>
2020-10-06 18:41:35 -0300

    Ajustes no dto

--- SME.Pedagogico.Gestao.Infra/Dtos/Relatorios/RelatorioImpressaoFiltroDto.cs
+++ SME.Pedagogico.Gestao.Infra/Dtos/Relatorios/RelatorioImpressaoFiltroDto.cs
@@ -3,8 +3,9 @@
     public class RelatorioImpressaoFiltroDto
     {
         public int AnoLetivo { get; set; }
-        public string DreCodigo { get; set; }
-        public string UeCodigo { get; set; }
+        public string Ano { get; set; }
+        public long DreCodigo { get; set; }
+        public long UeCodigo { get; set; }
         public int TurmaCodigo { get; set; }
         public ComponenteCurricularEnum ComponenteCurricular { get; set; }
         public ProficienciaEnum Proficiencia { get; set; }
