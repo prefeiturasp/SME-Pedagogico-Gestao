335af089caabd488a2a2961f531b4c53d1effd97
Everton Nogueira <everton.nogueira@amcom.com.br>
2020-10-19 11:30:30 -0300

    Aplicado configuração para utilizar secrets, encontrados varios pontos que utilizam variaveis de ambiente direto, necessário refatorar.

--- SME.Pedagogico.Gestao.WebApp/Program.cs
+++ SME.Pedagogico.Gestao.WebApp/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using System.Reflection;
 
 namespace SME.Pedagogico.Gestao.WebApp
 {
@@ -12,6 +14,18 @@ namespace SME.Pedagogico.Gestao.WebApp
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+            .ConfigureAppConfiguration((hostingContext, config) =>
+            {
+                var env = hostingContext.HostingEnvironment;
+                if (env.IsDevelopment())
+                {
+                    var appAssembly = Assembly.Load(new AssemblyName(env.ApplicationName));
+                    if (appAssembly != null)
+                    {
+                        config.AddUserSecrets(appAssembly, optional: true);
+                    }
+                }
+            })
                 .UseStartup<Startup>()
                 .UseUrls(urls: "http://0.0.0.0:80");
     }
