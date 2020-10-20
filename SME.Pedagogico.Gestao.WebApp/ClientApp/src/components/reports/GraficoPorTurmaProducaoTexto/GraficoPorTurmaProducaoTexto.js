f6ccddea200eb17a2f9e1aa6f35955bf3a840d82
eliton-amcom <eliton.santos@amcom.com.br>
2020-10-20 13:38:19 -0300

    26091 - Grafico producao de texto por turma

---
+++ SME.Pedagogico.Gestao.WebApp/ClientApp/src/components/reports/GraficoPorTurmaProducaoTexto/GraficoPorTurmaProducaoTexto.js
@@ -0,0 +1,74 @@
+import React, { useEffect } from "react";
+import echarts from 'echarts';
+
+const GraficoPorTurmaProducaoTexto = (props) => {
+  const { dados } = props;
+
+  useEffect(() => construirGrafico(), []);
+
+  const construirGrafico = () => {
+    const myChart = echarts.init(
+      document.getElementById(`grafico-producao-texto`)
+    );
+
+    const dadosLabel = [];
+    const dadosValores = [];
+
+    dados.barras.forEach((item) => {
+      dadosLabel.push(item.label);
+      dadosValores.push(item.value);
+    });
+
+    myChart.setOption({
+      tooltip: {},
+      xAxis: {
+        type: "category",
+        data: dadosLabel,
+      },
+      yAxis: {
+        type: "value",
+      },
+      series: [
+        {
+          data: dadosValores,
+          type: "bar",
+          showBackground: true,
+          itemStyle: {
+            normal: {
+              color: "#9C96F6",
+            },
+          },
+          backgroundStyle: {
+            color: "rgba(220, 220, 220, 0.8)",
+          },
+        },
+      ],
+    });
+  };
+
+  return (
+    <div className="d-flex flex-column col-12">
+      <div
+        className="d-flex justify-content-center align-items-center sc-gray"
+        style={{ height: 35 }}
+      >
+        <div className="sc-text-size-1 font-weight-bold">
+          {dados.nomeGrafico}
+        </div>
+      </div>
+      <div
+        className="d-flex flex-fill justify-content-center"
+        style={{ position: "relative", top: -35 }}
+      >
+        <div>
+          <div
+            id={`grafico-producao-texto`}
+            style={{ height: 400, width: 1024 }}
+          ></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default GraficoPorTurmaProducaoTexto;
