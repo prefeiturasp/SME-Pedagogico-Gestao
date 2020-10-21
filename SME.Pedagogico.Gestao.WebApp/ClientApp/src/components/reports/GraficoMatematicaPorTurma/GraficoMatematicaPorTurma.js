f651ef4dbd7cfeb1e96c172de7bfa1f1da6da53e
eliton-amcom <eliton.santos@amcom.com.br>
2020-10-21 13:22:27 -0300

    21737 - ajuste useeffect dados grafico matematica por turma

--- SME.Pedagogico.Gestao.WebApp/ClientApp/src/components/reports/GraficoMatematicaPorTurma/GraficoMatematicaPorTurma.js
+++ SME.Pedagogico.Gestao.WebApp/ClientApp/src/components/reports/GraficoMatematicaPorTurma/GraficoMatematicaPorTurma.js
@@ -5,7 +5,7 @@ import echarts from "echarts";
 const GraficoMatematicaPorTurma = (props) => {
   const { dados, index } = props;
 
-  useEffect(() => construirGrafico(), []);
+  useEffect(() => construirGrafico(), [dados]);
 
   const construirGrafico = () => {
     const myChart = echarts.init(document.getElementById(`grafico-${index}`));
@@ -13,10 +13,10 @@ const GraficoMatematicaPorTurma = (props) => {
     const dadosLabel = [];
     const dadosValores = [];
 
-    dados.barras.forEach(item => {
-        dadosLabel.push(item.label);
-        dadosValores.push(item.value);
-    })
+    dados.barras.forEach((item) => {
+      dadosLabel.push(item.label);
+      dadosValores.push(item.value);
+    });
 
     myChart.setOption({
       tooltip: {},
@@ -51,14 +51,19 @@ const GraficoMatematicaPorTurma = (props) => {
         className="d-flex flex-fill justify-content-center align-items-center sc-gray"
         style={{ height: 35 }}
       >
-        <div className="sc-text-size-1 font-weight-bold">{dados.nomeGrafico}</div>
+        <div className="sc-text-size-1 font-weight-bold">
+          {dados.nomeGrafico}
+        </div>
       </TituloGrafico>
       <div
         className="d-flex flex-fill justify-content-center"
         style={{ position: "relative", top: -35 }}
       >
         <div>
-          <div id={`grafico-${index}`} style={{ height: 400, width: 517 }}></div>
+          <div
+            id={`grafico-${index}`}
+            style={{ height: 400, width: 517 }}
+          ></div>
         </div>
       </div>
     </div>
