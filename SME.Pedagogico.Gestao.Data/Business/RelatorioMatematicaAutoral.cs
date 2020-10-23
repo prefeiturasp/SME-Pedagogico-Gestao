ed78d507ce772854d36635c96370fc1d19a20406
Alexsander Camargo <53785959+AlexsanderAMcom@users.noreply.github.com>
2020-10-23 09:52:34 -0300

    Update RelatorioMatematicaAutoral.cs

--- SME.Pedagogico.Gestao.Data/Business/RelatorioMatematicaAutoral.cs
+++ SME.Pedagogico.Gestao.Data/Business/RelatorioMatematicaAutoral.cs
@@ -149,7 +149,7 @@ namespace SME.Pedagogico.Gestao.Data.Business
             {
                 var aluno = new AlunoPorTurmaRelatorioDTO();
                 aluno.CodigoAluno = alunoRetorno.CodigoAluno;
-                aluno.NomeAluno = alunoRetorno.NomeAluno;
+                aluno.NomeAluno = alunoRetorno.NomeAlunoRelatorio;
                 aluno.Perguntas = new List<PergurntaRespostaPorAluno>();
 
                 var alunoRespostas = AlunosAgrupados.Where(x => x.Key == aluno.CodigoAluno.ToString()).ToList();
