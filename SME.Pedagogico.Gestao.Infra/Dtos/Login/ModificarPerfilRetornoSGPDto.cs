5959ac460710f352b66a4711b3778d789afa26ee
Marcos Lobo <marcos.lobo.nit@gmail.com>
2020-11-06 18:03:40 -0300

    Removendo campos não utilizados

--- SME.Pedagogico.Gestao.Infra/Dtos/Login/ModificarPerfilRetornoSGPDto.cs
+++ SME.Pedagogico.Gestao.Infra/Dtos/Login/ModificarPerfilRetornoSGPDto.cs
@@ -5,11 +5,8 @@ namespace SME.Pedagogico.Gestao.Infra
     public class ModificarPerfilRetornoSGPDto
     {
         public bool EhProfessor { get; set; }
-        public bool EhProfessorCj { get; set; }
-        public bool EhProfessorPoa { get; set; }
-        public DateTime DataHoraExpiracao { get; set; }
+        
         public string Token { get; set; }
-        public bool EhProfessorCjInfantil { get; set; }
-        public bool EhProfessorInfantil { get; set; }
+        
     }
 }
