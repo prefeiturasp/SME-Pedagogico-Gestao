b3f60b51c36bf0a5b1f35ba011dc0f9965830adb
Marcos Lobo <marcos.lobo.nit@gmail.com>
2020-11-07 14:50:42 -0300

    Ajustando enumerados

--- SME.Pedagogico.Gestao.Aplicacao/Queries/ComponentesCurriculares/ObterCCPorTurmaUsuario/ObterCCPorTurmaUsuarioQueryHandler.cs
+++ SME.Pedagogico.Gestao.Aplicacao/Queries/ComponentesCurriculares/ObterCCPorTurmaUsuario/ObterCCPorTurmaUsuarioQueryHandler.cs
@@ -1,8 +1,10 @@
 ﻿using MediatR;
 using Newtonsoft.Json;
 using SME.Pedagogico.Gestao.Data.Integracao.DTO.RetornoNovoSGP;
+using SME.Pedagogico.Gestao.Infra;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Threading;
@@ -14,6 +16,7 @@ namespace SME.Pedagogico.Gestao.Aplicacao
     {
         private readonly IHttpClientFactory httpClientFactory;
         private readonly IMediator mediator;
+
         public ObterCCPorTurmaUsuarioQueryHandler(IHttpClientFactory httpClientFactory, IMediator mediator)
         {
             this.httpClientFactory = httpClientFactory ?? throw new ArgumentNullException(nameof(httpClientFactory));
@@ -21,31 +24,51 @@ namespace SME.Pedagogico.Gestao.Aplicacao
         }
         public async Task<IList<DisciplinaRetornoDto>> Handle(ObterCCPorTurmaUsuarioQuery request, CancellationToken cancellationToken)
         {
-            var token = await mediator.Send(new ObterObterTokenUsuarioLogadoQuery());
+            var componenteMat = new DisciplinaRetornoDto() { CodigoComponenteCurricular = "math", Nome = "Matemática" };
+            var componentePor = new DisciplinaRetornoDto() { CodigoComponenteCurricular = "port", Nome = "Língua Portuguesa" };
 
-            using (var httpClient = httpClientFactory.CreateClient("apiSGP"))
+            var listFixaComponentes = new List<DisciplinaRetornoDto>() { componenteMat, componentePor };
+
+            var perfilAtual = await mediator.Send(new ObterPerfilUsuarioLogadoQuery());
+
+            if (perfilAtual.ToUpper() == "40E1E074-37D6-E911-ABD6-F81654FE895D")
             {
-                httpClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {token}");
-                var resposta = await httpClient.GetAsync($"v1/professores/turmas/{request.TurmaCodigo}/disciplinas");
+                var token = await mediator.Send(new ObterObterTokenUsuarioLogadoQuery());
 
-                if (!resposta.IsSuccessStatusCode || resposta.StatusCode == HttpStatusCode.NoContent)
+                using (var httpClient = httpClientFactory.CreateClient("apiSGP"))
                 {
-                    return null;
+                    httpClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {token}");
+                    var resposta = await httpClient.GetAsync($"v1/professores/turmas/{request.TurmaCodigo}/disciplinas");
+
+                    if (!resposta.IsSuccessStatusCode || resposta.StatusCode == HttpStatusCode.NoContent)
+                    {
+                        return null;
+                    }
+
+                    var json = await resposta.Content.ReadAsStringAsync();
+
+                    var listaComponentesSGP = JsonConvert.DeserializeObject<List<DisciplinaRetornoDto>>(json);
+
+                    //Verificar se tem regencia, se tiver, deve visualizar PT e MT, caso não, verifico se tem MT e PT para deixar na lista
+                    var listaIdsEnum = EnumExtensao.Listar<ComponentesCurricularesRegenciaEnum>().Select(a => (int)a);
+
+
+                    if (!listaComponentesSGP.Any(a => listaIdsEnum.Contains(int.Parse(a.CodigoComponenteCurricular))))
+                    {
+                        //Verifico se tem Mat
+                        if (!listaComponentesSGP.Any(a => a.CodigoComponenteCurricular == "2"))
+                            listFixaComponentes.Remove(componenteMat);
+
+                        //Verifico se tem Pt
+                        if (!listaComponentesSGP.Any(a => a.CodigoComponenteCurricular == "138"))
+                            listFixaComponentes.Remove(componentePor);
+
+                    }
                 }
-
-                var json = await resposta.Content.ReadAsStringAsync();
-
-                var listaComponentes =  JsonConvert.DeserializeObject<List<DisciplinaRetornoDto>>(json);
-
-                var listFixaComponentes = new List<DisciplinaRetornoDto>() 
-                { 
-                    new DisciplinaRetornoDto() { CodigoComponenteCurricular = "math", Nome = "Matemática" }, 
-                    new DisciplinaRetornoDto() { CodigoComponenteCurricular = "port", Nome = "Língua Portuguesa" }
-                };                
-
-                return listFixaComponentes;
-
             }
+
+
+            return listFixaComponentes;
         }
     }
 }
