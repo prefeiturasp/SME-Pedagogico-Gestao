37b7f2f58f67416d8406b50a8402076e9445f11a
Robson <robson.silva@amcom.com.br>
2021-11-17 12:31:54 -0300

    Bug: 51445 Ajuste para detectar o bug de login.

--- SME.Pedagogico.Gestao.WebApp/Middlewares/FiltroExcecoesAttribute.cs
+++ SME.Pedagogico.Gestao.WebApp/Middlewares/FiltroExcecoesAttribute.cs
@@ -15,7 +15,8 @@ namespace SME.Pedagogico.Gestao.WebApp.Middlewares
                     context.Result = new ResultadoBaseResult(context.Exception.Message, negocioException.StatusCode);
                     break;
                 case ValidacaoException validacaoException:
-                    context.Result = new ResultadoBaseResult(new RetornoBaseDto(validacaoException.Erros));
+                    //context.Result = new ResultadoBaseResult(new RetornoBaseDto(validacaoException.Erros));
+                    context.Result = new ResultadoBaseResult($"Erro de validação. Detalhes: {validacaoException}");
                     break;
                 default:
                     context.Result = new ResultadoBaseResult($"Ocorreu um erro interno. Favor contatar o suporte. {context.Exception}", 500);
