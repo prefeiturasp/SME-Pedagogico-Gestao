fda396d9346df837feeb52f12850c95f1bb97b00
Thiago Fernando <thiago.fernando@msn.com>
2022-02-18 18:37:57 -0300

    Update V10__INCLUIR_BIMESTRE_SONDAGEM.sql
    add sondagem

--- scripts/V10__INCLUIR_BIMESTRE_SONDAGEM.sql
+++ scripts/V10__INCLUIR_BIMESTRE_SONDAGEM.sql
@@ -1,2 +1,3 @@
 alter table "SondagemAlunoRespostas" add column if not exists "Bimestre" int4;
 alter table "SondagemAluno" add column if not exists "Bimestre" int4;
+alter table "Sondagem" add column if not exists "Bimestre" int4;
