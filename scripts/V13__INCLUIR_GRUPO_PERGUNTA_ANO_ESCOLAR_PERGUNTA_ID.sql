156d7574ff75ee30716c7147fc2f113f096c4207
Danilo Ferreira <shymmi@hotmail.com>
2022-03-03 10:25:14 -0300

    inclusão de do "Drop column if exists antes de add"

--- scripts/V13__INCLUIR_GRUPO_PERGUNTA_ANO_ESCOLAR_PERGUNTA_ID.sql
+++ scripts/V13__INCLUIR_GRUPO_PERGUNTA_ANO_ESCOLAR_PERGUNTA_ID.sql
@@ -1,3 +1,6 @@
-alter table public."PerguntaAnoEscolar" add column if not exists "Grupo" int;
 
-alter table public."Pergunta" add column if not exists "PerguntaId" text null;
+alter table public."PerguntaAnoEscolar" drop column if exists "Grupo";
+alter table public."PerguntaAnoEscolar" add column "Grupo" int;
+
+alter table public."Pergunta" drop column if exists "PerguntaId";
+alter table public."Pergunta" add column "PerguntaId" text null;
