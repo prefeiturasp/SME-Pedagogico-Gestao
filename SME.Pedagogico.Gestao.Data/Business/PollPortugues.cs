8d8f1a71d405a4fb7b220a28ef09d55948b73cc1
Thiago Fernando <thiago.fernando@msn.com>
2023-05-02 13:50:17 -0300

    Correção totalSemPreenchimento

--- SME.Pedagogico.Gestao.Data/Business/PollPortugues.cs
+++ SME.Pedagogico.Gestao.Data/Business/PollPortugues.cs
@@ -481,7 +481,7 @@ namespace SME.Pedagogico.Gestao.Data.Business
 
                 int totalSemPreenchimento = (quantidadeTotalAlunos - listaGrafico.Where(l => !string.IsNullOrWhiteSpace(l.Label)).Sum(l => l.Value)) >= 0
                     ? quantidadeTotalAlunos - listaGrafico.Where(l => !string.IsNullOrWhiteSpace(l.Label)).Sum(l => l.Value)
-                    : listaGrafico.Where(l => string.IsNullOrWhiteSpace(l.Label)).Count();
+                    : 0;
 
                 foreach (var item in listReturn)
                 {
