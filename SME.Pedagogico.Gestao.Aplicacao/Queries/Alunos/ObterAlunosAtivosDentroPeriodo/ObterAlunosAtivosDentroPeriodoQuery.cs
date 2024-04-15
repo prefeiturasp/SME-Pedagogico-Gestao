7ec1e9bcc60a104366f413fa0f7e4878b069e78c
Robson Oliveira da Silva <robson.silva@amcom.com.br>
2024-04-15 15:51:26 -0300

    Bug: 119494 Ajuste na listagem de alunos na digitação de matemática equalizando ao selecionar CA e CM com a listagem mostrada em números coniderando o período selecionado não só a data fim. Equalização da lista de alunos na digitação de matemática em relação a português considerando alunos inativos para verificar se estão dentro do período.

---
+++ SME.Pedagogico.Gestao.Aplicacao/Queries/Alunos/ObterAlunosAtivosDentroPeriodo/ObterAlunosAtivosDentroPeriodoQuery.cs
@@ -0,0 +1,36 @@
+﻿using FluentValidation;
+using MediatR;
+using SME.Pedagogico.Gestao.Data.Integracao.DTO.RetornoQueryDTO;
+using System;
+using System.Collections.Generic;
+
+namespace SME.Pedagogico.Gestao.Aplicacao
+{
+    public class ObterAlunosAtivosDentroPeriodoQuery : IRequest<IEnumerable<AlunosNaTurmaDTO>>
+    {
+        public ObterAlunosAtivosDentroPeriodoQuery(string codigoTurma, int anoLetivo, (DateTime inicio, DateTime fim)? periodo)
+        {
+            CodigoTurma = codigoTurma;
+            AnoLetivo = anoLetivo;
+            Periodo = periodo ?? (DateTime.Now.Date, DateTime.Now.Date);            
+        }
+
+        public string CodigoTurma { get; set; }
+        public int AnoLetivo { get; set; }
+        public (DateTime inicio, DateTime fim) Periodo { get; set; }        
+    }
+
+    public class ObterAlunosAtivosDentroPeriodoQueryValidator : AbstractValidator<ObterAlunosAtivosDentroPeriodoQuery>
+    {
+        public ObterAlunosAtivosDentroPeriodoQueryValidator()
+        {
+            RuleFor(x => x.CodigoTurma)
+                .NotEmpty()
+                .WithMessage("O código da turma deve ser informado.");
+
+            RuleFor(x => x.AnoLetivo)
+                .InclusiveBetween(2014, DateTime.Now.Year)
+                .WithMessage("O ano letivo é inválido.");
+        }
+    }
+}
